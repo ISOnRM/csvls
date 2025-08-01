364f4861e86b5dfd438fe6bc69f0205027a712ce
ISOnRM <markison7002@gmail.com>
2025-08-02 00:53:22 +0300

    Add include.hpp in src/

---
+++ src/include.hpp
@@ -0,0 +1,25 @@
+// All modules
+#ifndef INLUCDE_HPP
+#define INLUCDE_HPP
+
+// aliases_and_concepts
+#include "aliases_and_concepts/concept.hpp"
+#include "aliases_and_concepts/using.hpp"
+
+// assembler
+#include "assembler/DirWrapper.hpp"
+#include "assembler/Assembler.hpp"
+
+// checker
+#include "checker/ArgumentChecker.hpp"
+#include "checker/help.hpp"
+
+// cli
+#include "cli/ArgumentParser.hpp"
+#include "cli/Option.hpp"
+#include "cli/OrderedUnique.hpp"
+
+// csv_writer
+#include "csv_writer/CsvWriter.hpp"
+
+#endif
