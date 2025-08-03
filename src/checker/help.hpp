259e7b60a639e08e386d068d305b50daee1dbd1e
ISOnRM <markison7002@gmail.com>
2025-08-03 17:25:55 +0300

    Minor changes in checker/help.hpp 2

--- src/checker/help.hpp
+++ src/checker/help.hpp
@@ -6,8 +6,8 @@
 
 
 inline void print_help() {
-    std::cerr << "Usage:\n";
-    std::cerr << "  program [options] <targets>...\n\n";
+	std::cerr << "Usage:\n";
+    std::cerr << "  csvls [options] <targets>...\n\n";
     std::cerr << "Description:\n";
     std::cerr << "  Targets are given without a leading \"-\" and must be existing directories or files to scan.\n\n";
     std::cerr << "Options:\n";
@@ -18,8 +18,8 @@ inline void print_help() {
     std::cerr << "  -t, --show-type      Display file type.\n";
     std::cerr << "  -p, --show-perms     Display human readable file permissions.\n";
     std::cerr << "  -n, --show-nlinks    Display number of hard links.\n";
-    std::cerr << "  -O, --show-owner     Display owner's user ID.\n";
-    std::cerr << "  -g, --show-group     Display group ID.\n";
+    std::cerr << "  -O, --show-owner     Display owner's username.\n";
+    std::cerr << "  -g, --show-group     Display group's name.\n";
     std::cerr << "  -s, --show-size      Display size in human readable units.\n";
     std::cerr << "  -b, --show-blocks    Display number of blocks.\n";
     std::cerr << "  -a, --show-access-time Display last access time.\n";
@@ -28,6 +28,7 @@ inline void print_help() {
     std::cerr << "  -N, --name           Display entry name.\n";
     std::cerr << "  -c, --canonical      Display canonical path.\n";
     std::cerr << "  -S, --sort           Sort output by selected options.\n";
-    std::cerr << "  -r, --recursive      Enable recursive directory traversal.\n";
+    std::cerr << "  -r, --recursive      Enable recursive directory traversal.\n\n";
+	std::cerr << "  Look up command examples on github ISOnRM/csvls\n";
 }
 #endif
